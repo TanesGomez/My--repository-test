ab8d40b976803d13465c9c7e31ba0fcf41767d20
TanesGomez <Tanesvillagomez2@gmail.com>
2025-05-28 10:59:31 -0500

    Actualizacion rama

--- Nombre.py
+++ Nombre.py
@@ -1,3 +1,3 @@
 Nombre  =   "Jhonathan"
 Apellido = "Villa" 
-print("Hola, mi nombre es", Nombre + " " + Apellido, "¿Como estás?")
+print("Hola, mi nombre es", Nombre + " " + Apellido, "¿Como está usted el día de hoy?")
